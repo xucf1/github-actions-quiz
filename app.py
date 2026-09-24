15374cad665038082e8933f8f185a70128697da2
chengfei xu <83813128+xucf1@users.noreply.github.com>
2026-09-24 17:21:49 +0800

    Create app.py
    Add Flask backend

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return {"message": "GitHub Actions Training Quiz"}
+
+
+@app.route("/health")
+def health():
+    return {"status": "ok"}
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
